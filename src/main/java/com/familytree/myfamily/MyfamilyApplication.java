59a06ce1331a1c5593448ea3c36eb5089d2affca
Vamshi Gunturu <vamshi@MacBook-Pro.attlocal.net>
2020-07-01 21:01:35 -0500

    Updated with html

--- src/main/java/com/familytree/myfamily/MyfamilyApplication.java
+++ src/main/java/com/familytree/myfamily/MyfamilyApplication.java
@@ -3,6 +3,7 @@ package com.familytree.myfamily;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MyfamilyApplication {
 
--- src/main/java/com/familytree/myfamily/controller/MyMainFamilyController.java
+++ src/main/java/com/familytree/myfamily/controller/MyMainFamilyController.java
@@ -1,13 +1,37 @@
 package com.familytree.myfamily.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Arrays;
+import java.util.List;
 
-@RestController
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class MyMainFamilyController {
 
-	@GetMapping("/helloworld")
-	public String hello() {
-		return "Hello World!";
+	@Value("${welcome.message}")
+	private String message;
+
+	private List<String> roles = Arrays.asList("Grand Father", "Grand Mother", "Father", "Mother", "Brother", "Sister", "Uncle");
+
+	@GetMapping("/")
+	public String main(Model model) {
+		model.addAttribute("message", message);
+		model.addAttribute("roles", roles);
+
+		return "welcome";
 	}
+
+	@GetMapping("/hello")
+	public String mainWithParam(@RequestParam(name = "name", required = false, defaultValue = "") String name,
+			Model model) {
+
+		model.addAttribute("message", name);
+
+		return "welcome";
+	}
+
 }
